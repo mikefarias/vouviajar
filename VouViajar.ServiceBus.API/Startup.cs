e83c1de9a84eed3e348b1638df367bc40b583b15
4042190 <mike@protechsolutions.com.br>
2021-10-24 14:37:13 -0300

    Configuração do Swagger

--- VouViajar.ServiceBus.API/Startup.cs
+++ VouViajar.ServiceBus.API/Startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,25 +26,55 @@ namespace VouViajar.ServiceBus.API
         {
             services.AddCatalogoModuleRegistrationService(Configuration);
             services.AddControllers().AddApplicationPart(Assembly.GetAssembly(typeof(CatalogoController)));
+            services.AddHttpClient();
 
+
+
+            #region Swagger
+            services.AddControllers()
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.SuppressModelStateInvalidFilter = true;
+                });
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc(name: "v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Vou Viajar - API Autenticação", Version = "v1" })
+            );
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            #endregion
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Vou Viajar - API Autenticação");
+                    c.RoutePrefix = string.Empty;
+                }
+                );
             }
 
+            app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseCors();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                //endpoints.MapHangfireDashboard();
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Seac service bus running...");
+                //});
             });
         }
     }
