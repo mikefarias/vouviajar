066b27b9ffdcbeee883389c8e4a82a09684ff59e
4042190 <mike@protechsolutions.com.br>
2022-02-25 15:06:48 -0300

    Validação de ID maior que zero

--- VouViajar.Modulos.Eventos/Application/Features/Commands/AtualizarEvento/AtualizarEventoValidator.cs
+++ VouViajar.Modulos.Eventos/Application/Features/Commands/AtualizarEvento/AtualizarEventoValidator.cs
@@ -6,7 +6,7 @@ namespace VouViajar.Modulos.Eventos.Application.Features.Commands.AtualizarEvent
     {
         public AtualizarEventoValidator()
         {
-            RuleFor(r => r.ID).NotEmpty().WithMessage("Necessário informar ID maior que zero.");
+            RuleFor(r => r.ID).GreaterThan(0).WithMessage("Necessário informar ID maior que zero.");
             RuleFor(r => r.Nome).NotEmpty().WithMessage("Necessário informar o nome do evento.");
             RuleFor(r => r.Resumo).NotEmpty().WithMessage("Necessário informar o reusmo do evento.");
             RuleFor(r => r.DataInicio).NotEmpty().WithMessage("Necessário informar a data de início do evento.");
